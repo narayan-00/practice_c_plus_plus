3b0350c7f582eba8ece9e4610936f7eacf08ca49
Narayan Kumar <108329162+narayan-00@users.noreply.github.com>
2023-05-04 03:10:34 +0530

    Update Palindrome Check String.cpp

--- Palindrome Check String.cpp
+++ Palindrome Check String.cpp
@@ -16,3 +16,37 @@ int main()
     cout<<isPalindrome(str);
     
 }
+_______________________________________________________________________________________________
+    (optimized)
+    
+  /******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(string str){
+    int begin=0,end=str.length()-1;
+    while(begin<end){
+        if(str[begin]!=str[end]){
+            return false;
+        }
+        begin++;
+        end--;
+    }
+    return true;
+}
+
+int main()
+{
+    // string str="ABBA";
+    string str="geeks skeeg";
+    cout<<isPalindrome(str);
+    
+}
